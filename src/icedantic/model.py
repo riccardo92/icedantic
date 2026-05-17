686babf35939a0d08ccdbe26a88c53f8290b33d8
Riccardo Tiebax <riccardo.t@gmail.com>
2026-05-17 18:40:18 +0000

    fix: iceberg types should now specified as strings; added exception for unknown types

--- src/icedantic/model.py
+++ src/icedantic/model.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from typing import Any, Dict, get_type_hints
 from uuid import UUID
 
+from loguru import logger
 from pydantic import (
     BaseModel,
     Field,
@@ -22,6 +23,8 @@ from pyiceberg.types import (
     TimestampType,
     UUIDType,
 )
+
+from icedantic.exceptions import IcebergTypeUnknown
 
 _PYDANTIC_ICEBERG_TYPE_MAPPING: dict = {
     int: IntegerType,
@@ -55,6 +58,9 @@ _ICEBERG_TYPE_NAME_MAPPING: dict = {
 }
 
 
+_ICEBERG_NAME_TYPE_MAPPING = {v: k for k, v in _ICEBERG_TYPE_NAME_MAPPING.items()}
+
+
 def IcebergField(
     *args,
     iceberg_type: IcebergType | None = None,
@@ -70,14 +76,22 @@ def IcebergField(
 
 class IcebergBaseModel(BaseModel):
     @classmethod
-    def model_iceberg_schema(cls, by_alias: bool = True) -> Dict[str, Any]:
+    def model_iceberg_schema(
+        cls,
+        by_alias: bool = True,
+        throw_on_unknown: bool = False,
+        attempt_mapping: bool = True,
+    ) -> Dict[str, Any]:
         """Generate the model schema that is compatible with Apache Iceberg.
 
         Args:
             by_alias (bool, optional): Whether to use aliased field names in the
                 schema or not. Defaults to True.
+            throw_on_unknown (bool, optional): Throw an exception if the specified
+                `iceberg_type` isn't known to icedantic. Defaults to False.
             attempt_mapping (bool, optional): Attempt to map pydantic type to
-                iceberg type in case no explicit `iceberg_type` was set.
+                iceberg type in case no explicit `iceberg_type` was set or
+                the `iceberg_type` was invalid. Defaults to True.
 
         Returns:
             Dict[str, Any] The model's Apache Iceberg mapped schema
@@ -92,21 +106,39 @@ class IcebergBaseModel(BaseModel):
 
             # Attempt to get explicit iceberg_type from json_schema_extra
             json_extra_schema = field_info.json_schema_extra or {}
-            field_type = json_extra_schema.get("iceberg_type")
+            field_type = json_extra_schema.get("iceberg_type", None)
+
+            if field_type is not None:
+                try:
+                    _ICEBERG_NAME_TYPE_MAPPING[field_type]
+                except KeyError:
+                    logger.warning(
+                        "`iceberg_type` was set but was not set to a icedantic-known type."
+                    )
+
+                    if throw_on_unknown:
+                        raise IcebergTypeUnknown(
+                            f"The iceberg type '{field_type}' is not known to icedantic. Throwing since `throw_on_unknown` was set to True."
+                        )
 
             # If no iceberg type was set explicitly,
             # attempt to map it
-            if field_type is None:
-                field_type = _PYDANTIC_ICEBERG_TYPE_MAPPING.get(
-                    type_hints[field_name], None
+            if field_type is None and attempt_mapping:
+                logger.warning(
+                    f"No `iceberg_type` set for field {field_name}. Attempting native mapping."
+                )
+                field_type = _ICEBERG_TYPE_NAME_MAPPING.get(
+                    _PYDANTIC_ICEBERG_TYPE_MAPPING.get(type_hints[field_name], None),
+                    None,
                 )
 
             # Finally, set type to unknown if all else failed
-            field_type = (
-                _ICEBERG_TYPE_NAME_MAPPING[field_type]
-                if field_type is not None
-                else "unknown"
-            )
+            if field_type is None:
+                if attempt_mapping:
+                    logger.warning(
+                        "A native mapping from pydantic to iceberg could not be made. Type will be set to 'unknown'."
+                    )
+                field_type = "unknown"
 
             schema[field_key] = field_type
 
